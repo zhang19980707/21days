6d5c192aff756dd4ba7b0cf34d64ed841f791217
zsk19980707 <845664960@qq.com>
2020-02-08 11:59:42 +0800

    第一次尝试一次性提交两个文件夹

---
+++ pcka/__init__.py
@@ -0,0 +1,6 @@
+name = 'pcka'
+print('__init__中的name', name)
+
+
+def pck_test_fun():
+    print("pck包中的pck_test_fun函数")
---
+++ pcka/test_module.py
@@ -0,0 +1,5 @@
+print("包里的一个模块")
+
+
+def test_module_fun():
+    print("测试模块中的函数")
